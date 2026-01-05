83c9eaed25b6d5d7e95078c6ba002fded2feac3a
Henry Zheng <zhengruiw02@gmail.com>
2026-01-05 15:05:09 +0800

    Revert SDIO clock to 48MHz. Revert target image device package of linkit-smart-7688 to remove esp32-sdio
    Signed-off-by: Henry Zheng <zhengruiw02@gmail.com>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -505,7 +505,7 @@ define Device/mediatek_linkit-smart-7688
   IMAGE_SIZE := 32448k
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := LinkIt Smart 7688
-  DEVICE_PACKAGES:= kmod-usb2 kmod-usb-ohci uboot-envtools kmod-mmc-mtk kmod-esp32-sdio
+  DEVICE_PACKAGES:= kmod-usb2 kmod-usb-ohci uboot-envtools kmod-mmc-mtk
   SUPPORTED_DEVICES += linkits7688 linkits7688d
 endef
 TARGET_DEVICES += mediatek_linkit-smart-7688
